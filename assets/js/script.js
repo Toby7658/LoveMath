c427f11a995290f2a4b66a661cd23b166fb6f507
Toby7658 <orlaghtobin@gmail.com>
2022-08-23 13:41:13 +0000

    Add function to addition button

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,15 +10,23 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame("addition");
 });
 
-function runGame() {
-    let num1 = Math.floor(Mat h.random() *24) + 1;
+function runGame(gameType) {
+    let num1 = Math.floor(Math.random() *24) + 1;
     let num2 = Math.floor(Math.random() *24) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+     }else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`
+     }
 }
 
 function checkAnswer() {
@@ -37,8 +45,11 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
-    
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById(`operand1`).textContent = operand1;
+    document.getElementById(`operand2`).textContent = operand2;
+    document.getElementById(`operator`).textContent = "+";
+
 }
 
 function displaySubtractQuestion() {
